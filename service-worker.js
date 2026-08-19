a7a4e1e225e0d9d5f8e39d89b74a53a3a3273035
Dabros-AI-Coder <github@grimoire-interactive.de>
2026-08-19 06:15:11 +0200

    PC Web App Installer

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'hacker-tycoon-v1';
+const APP_FILES = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/events.js',
+  './js/game.js',
+  './js/save.js',
+  './js/state.js',
+  './js/ui.js',
+  './manifest.json',
+  './assets/icon.svg'
+];
+
+self.addEventListener('install', (event)=>{
+  event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event)=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(
+      keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event)=>{
+  event.respondWith(
+    caches.match(event.request).then(cached=>cached || fetch(event.request))
+  );
+});
