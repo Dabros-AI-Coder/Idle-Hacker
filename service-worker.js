7587103fa1543fa7a9b0ce99bc5ed34e854fb1ea
Dabros-AI-Coder <github@grimoire-interactive.de>
2026-08-19 06:48:27 +0200

    chore: refresh PWA cache for fullscreen launch

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='idle-hacker-v2';
+const CACHE_NAME='idle-hacker-v3';
 const APP_FILES=['./','./index.html','./css/style.css','./js/events.js','./js/game.js','./js/save.js','./js/state.js','./js/ui.js','./manifest.json','./assets/icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_FILES)));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))));self.clients.claim()});
